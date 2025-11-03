10aaf80014529a6435da17b366e920c04d5ce5a7
eitan <eitanitzhakov@gmail.com>
2025-11-03 11:00:11 +0200

    adding augmentation layer. this makes random changes in the img to train the model better adding model.py - changing to the EfficientNetV2B0 model - more efficient

--- model/model.py
+++ model/model.py
@@ -1,12 +1,16 @@
-from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications import EfficientNetV2B0
 from tensorflow.keras import layers, models
+from tensorflow.keras.metrics import AUC
+
 class Model:
     def __init__(self, input_shape=(224,224,3), classes = 6):
         self.input_shape = input_shape
         self.classes = classes
+        self.model = self.__build_model()
+
 
     def __build_model(self):
-        base_model = DenseNet121(include_top=False, input_shape=self.input_shape, weights="imagenet", pooling=None)
+        base_model = EfficientNetV2B0(include_top=False, input_shape=self.input_shape, weights="imagenet", pooling=None)
         base_model.trainable = False
 
         x = base_model.output
@@ -18,6 +22,12 @@ class Model:
 
         return models.Model(input = base_model.input, output = output, name = 'Hemmorage_Bleeding_ClassificationModel')
 
-    def build_model(self):
-        model = self.__build_model()
+    def compile(self):
+        self.model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy", AUC(name = "auc")])
 
+    def summary(self):
+        return self.model.summary()
+
+    def predict(self, tensor):
+        prediction = self.model.predict(tensor)
+        return prediction
